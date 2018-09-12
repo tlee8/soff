1a4a283df3e263150b883fe3a187d908b96bd3e2
Thomas Lee <34113032+tlee8@users.noreply.github.com>
2018-09-12 19:17:57 -0400

    added 02_randomizer

---
+++ 02_randomizer.py
@@ -0,0 +1,19 @@
+#Thomas Lee
+#SoftDev1 pd06
+#K02 -- NO-body expects the Spanish Inquisition!
+#2018-09-12
+
+KREWES = {
+        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+}
+
+import random
+
+chosenTeam = raw_input("Choose your team: ") #User chooses the team from which they want a random member of
+
+if KREWES.has_key(chosenTeam):
+    print (KREWES[chosenTeam][random.randint(0, len(KREWES[chosenTeam]))]) #Uses the chosenTeam key to access the associated value. Then uses a random number as an index to ultimately get a random member of the team.
+else:
+    print ("KREWES doesn't have team " + chosenTeam) #For when the user inputs an invalid input
