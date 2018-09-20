3999b3edbde2b1e3d4c1e5f95f7588a595cf8108
Thomas Lee <34113032+tlee8@users.noreply.github.com>
2018-09-20 18:52:20 -0400

    Added updated version with heading and K09

---
+++ 08_lemme_flask_u_sumpn/app.py
@@ -0,0 +1,25 @@
+#Thomas Lee
+#SoftDev1 pd6
+#K 08 -- Fill Yer Flask
+#2018-09-20
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/") 
+def juice__world():
+    print (__name__)
+    return "<a href='/juice'> Juice Wrld </a>"
+
+@app.route("/juice")
+def lucid_dreams():
+    print (__name__)
+    return "<a href='/lyrics'> Lucid Dreams</a> by <a href='/'>Juice Wrld </a>"
+
+@app.route("/lyrics")
+def lyrics():
+    print (__name__)
+    return "I have these <a href='/juice'> lucid dreams</a> where I can't move a thing"
+
+app.debug = True
+app.run()
