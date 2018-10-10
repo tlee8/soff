b37b94e5daa88aa15c917d00a11bed345cf3196b
tlee8 <tlee8@stuy.edu>
2018-10-09 22:01:32 -0400

    updated k17

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -9,20 +9,20 @@ db = sqlite3.connect("discobandit.db") #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
 def stu_meaner(list1,list2):
-    c.execute("DROP TABLE IF EXISTS peeps_avg;")
+    c.execute("DROP TABLE IF EXISTS peeps_avg;")#removes existing table, otherwise creates
     c.execute("CREATE TABlE peeps_avg (id TEXT, avg TEXT)")
-    idlist = list(c.execute( "SELECT id FROM "+list1))
+    idlist = list(c.execute( "SELECT id FROM "+list1))#assembles all ids
     id_list = []
     for el in idlist:
         id_list.append(el[0])
     for ide in id_list:
         print ("==========================================" + "\n")
-        command = "SELECT name FROM {} WHERE id = ".format(list1)
+        command = "SELECT name FROM {} WHERE id = ".format(list1)#matches names to id
         command += ide
         name = (list(c.execute(command))[0][0])
         print (name)
         print (ide)
-        command = "SELECT mark FROM {} WHERE id = ".format(list2)
+        command = "SELECT mark FROM {} WHERE id = ".format(list2)#gets grades from id
         command += ide
         gradelist = []
         gradelist = list(c.execute (command))
@@ -32,19 +32,26 @@ def stu_meaner(list1,list2):
             grade_list.append(int(el[0]))
         #print (grade_list)
         avg = 0
-        avg = str(sum(grade_list) / len(grade_list))
+        avg = str(sum(grade_list) / len(grade_list))#calculates avg for each person
         print (avg + "\n")
+        params = (ide, avg)
+        c.execute("INSERT INTO peeps_avg VALUES (?,?)", params) #adds avgs and ids to peeps_avg
+        '''
         command = "INSERT INTO peeps_avg VALUES("
         command += ide + ", "
         command += avg
         command += ")"
         #print(command)
         c.execute(command)
+        '''
         
-def addRow(list_of,code,mark,id_num):
-    c.execute("INSERT INTO {} VALUES('{}',{},{})".format(list_of,code,mark,id_num)) 
+def addRowToCourses(code,mark,id_num):
+    command = "INSERT INTO course_list VALUES(?,?,?)"#adds a new row with these values
+    c.execute(command,(code,mark,id_num)) 
 stu_meaner("peep_list","course_list")
-addRow("course_list","systems",95,2)
+addRowToCourses("systems",95,2)
+addRowToCourses("softdev",60,5)
+addRowToCourses("ai",75,9)
 print("==================NEW TABLE=============")
 stu_meaner("peep_list","course_list")
 db.commit()
